363ea39d34b97a76d4af74bfa82c67ba44f15d8b
xulay <5052936@qq.com>
2017-09-08 15:27:13 +0800

    Site updated: 2017-09-08 15:27:13

--- photos/photos.js
+++ photos/photos.js
@@ -3,7 +3,9 @@ var render = function(page, data){
 	for (var i = 0; i < data.length; i++) {
 		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img class="lazy" data-original="http://oquhznm5y.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
 		//img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
+		console.log(data)
 	}
+	console.log(page)
 	$(".img-box-ul").append(img);
 	$(".img-box-ul").lazyload();
 	$("a[rel=example_group]").fancybox();
