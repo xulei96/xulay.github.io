42439627217fd6c2a3c1fbb3f0187c4ec993e6ca
xulay <5052936@qq.com>
2017-09-08 14:42:35 +0800

    Site updated: 2017-09-08 14:42:35

--- photos/photos.js
+++ photos/photos.js
@@ -1,17 +1,10 @@
-var render = function(page, data){ 
-	console.log(data);
-	console.log(typeof data);
+var render = function(page, data){
 	var img = "";
-	$.each(data,function(name,value) {
-		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + value + '" target="_blank"></a>' + '<img lazy-src="http://oquhznm5y.bkt.clouddn.com/' + value + '">'  + '</div></li>';
-		alert(1)
-			//img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
-	});
-	img = '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + '0001.jpg' + '" target="_blank"></a>' + '<img class="lazy" data-original="http://oquhznm5y.bkt.clouddn.com/' + '0001.jpg' + '">'  + '</div></li>';
-
-	
+	for (var i = 0; i < data.length; i++) {
+		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img lazy-src="http://oquhznm5y.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
+		//img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
+	}
 	$(".img-box-ul").append(img);
 	$(".img-box-ul").lazyload();
 	$("a[rel=example_group]").fancybox();
 }
-
