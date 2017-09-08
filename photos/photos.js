de768cb0b4743ca849e2c74a66110f05e89932f8
xulay <5052936@qq.com>
2017-09-08 14:19:21 +0800

    Site updated: 2017-09-08 14:19:21

--- photos/photos.js
+++ photos/photos.js
@@ -1,29 +1,17 @@
 var render = function(page, data){ 
-	getJsonLength(data)
 	console.log(data);
 	console.log(typeof data);
-	console.log(jsonLength);
 	var img = "";
 	$.each(data,function(name,value) {
 		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + value + '" target="_blank"></a>' + '<img lazy-src="http://oquhznm5y.bkt.clouddn.com/' + value + '">'  + '</div></li>';
+		alert(1)
 			//img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
 	});
+	img = '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + '0001.jpg' + '" target="_blank"></a>' + '<img lazy-src="http://oquhznm5y.bkt.clouddn.com/' + '0001.jpg' + '">'  + '</div></li>';
 
 	
 	$(".img-box-ul").append(img);
 	$(".img-box-ul").lazyload();
 	$("a[rel=example_group]").fancybox();
 }
-function getJsonLength(jsonData){  
-	  
-	    var jsonLength = 0;  
-	  
-	    for(var item in jsonData){  
-	  
-	        jsonLength++;  
-	  
-	    }  
-	  
-	    return jsonLength;  
-	  
-	} 
+
