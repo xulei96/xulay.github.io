e370715c50226cb41f7675e9009deced9d633575
xulay <5052936@qq.com>
2017-09-09 16:17:41 +0800

    Site updated: 2017-09-09 16:17:40

--- photos/photos.js
+++ photos/photos.js
@@ -2,7 +2,6 @@ var render = function(page, data){
 	var img = "";
 	for (var i = 0; i < data.length; i++) {
 		img += '<li><div class="img-box">' + '<a class="img-bg " rel="external" data-fancybox="images" href="http://oquhznm5y.bkt.clouddn.com/' + data[i] + '" target="_blank"></a>' + '<img class="lazy" data-original="http://oquhznm5y.bkt.clouddn.com/' + data[i] + '">'  + '</div></li>';
-		//img += '<img src="http://yourqiniu.url.com/' + data[i] + '">';
 	}
 	$(".img-box-ul").append(img);
 	$(".img-box-ul").lazyload();
